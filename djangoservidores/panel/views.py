50339b6bfefe108f558230c8e4414a509f6d6ed3
IvanGuGon1 <ivangugon1@gmail.com>
2017-11-15 17:32:34 +0100

    Cambiada la vista basada en clase por vista basada en funcion -> ServidoresList. A falta de ping

--- djangoservidores/panel/views.py
+++ djangoservidores/panel/views.py
@@ -7,9 +7,31 @@ from django.core.urlresolvers import reverse_lazy
 from panel.models import Servidores
 
 
+def ServidoresList(request):
+	servidores = Servidores.objects.all()
+	"""
+	Lanzar un ping. Si true, enviar la informacion al template para mostar On y Off
+	Opcion 1
+	import subprocess, platform
+	def pingOk(sHost):
+    	try:
+        	output = subprocess.check_output("ping -{} 1 {}".format('n' if platform.system().lower()=="windows" else 'c', sHost), shell=True)
+
+    	except Exception:
+        	return False
+
+    	return True
+	
+    	import os, sys
+		host = "8.8.8.8"
+		os.system("ping " + ("-n 1 " if  sys.platform().lower()=="win32" else "-c 1 ") + host)
+	"""
+	data = {}
+	data['object_list'] = servidores
+	return render(request, "servidores_list.html", data)
 
 class ServidoresList(ListView):
-	
+
     model = Servidores
 
 class ServidoresCreate(CreateView):
